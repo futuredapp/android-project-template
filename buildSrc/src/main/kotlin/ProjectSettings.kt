bc7a05a58b8503a960ed0bef6e234e3c083f5159
Lukas Sztefek <lukas.sztefek@gmail.com>
2021-10-04 15:59:53 +0200

    Bump compile & target SDK version to 31

--- buildSrc/src/main/kotlin/ProjectSettings.kt
+++ buildSrc/src/main/kotlin/ProjectSettings.kt
@@ -1,7 +1,7 @@
 object ProjectSettings {
     const val applicationId = "app.futured.androidprojecttemplate"
-    const val compileSdkVersion = 30
-    const val targetSdk = 30
+    const val compileSdkVersion = 31
+    const val targetSdk = 31
     const val minSdk = 21
     const val versionName = "1.0.0"
     val versionCode = System.getenv("GITHUB_RUN_NUMBER")?.toInt() ?: 1
